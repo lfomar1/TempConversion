f8734d99c2d77dca49fd575feb377d6a525d5e2d
Lucas <107668539+lfomar1@users.noreply.github.com>
2024-01-23 12:23:38 -0300

    Initial Commit

---
+++ TempConversion/TempConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConversionApp.swift
+//  TempConversion
+//
+//  Created by Lucas Omar on 2024-01-23.
+//
+
+import SwiftUI
+
+@main
+struct TempConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
